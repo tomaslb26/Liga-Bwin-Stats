fcfa2cf7af40b9ccba1eba72b093ac338442c9ee
tomaslb26 <tomaslb26@gmail.com>
2023-01-17 14:35:56 +0000

    fixed problem with scores

--- src/components/GetScore.js
+++ src/components/GetScore.js
@@ -2,16 +2,12 @@ export default function GetScore(data, home_id, away_id) {
     var own_goals = data.filter((item) => (item.isOwnGoal === "True"))
 
     var home_goals = data.filter((item) => ((item.type === "Goal" && Number(item.teamId) === home_id)))
-
-
     var away_goals = data.filter((item) => ((item.type === "Goal" && Number(item.teamId) === away_id)))
-
-    console.log(home_goals)
-
+    
     var home_own_goals = 0;
     var away_own_goals = 0;
     for (var i = 0; i < own_goals.length; i++) {
-        if (own_goals[i].teamId === home_id) {
+        if (Number(own_goals[i].teamId) === Number(home_id)) {
             home_own_goals -= 1
             away_own_goals += 1
         }
